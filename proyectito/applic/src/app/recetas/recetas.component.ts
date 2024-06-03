36b6f1c48a8918c4a7ca2a241bacfae8067f7ab6
Jesus Emanuel Palacios Mendias <jpalaciosmendias0@gmail.com>
2024-06-02 18:24:16 -0600

    FINAL

--- proyectito/applic/src/app/recetas/recetas.component.ts
+++ proyectito/applic/src/app/recetas/recetas.component.ts
@@ -2,11 +2,13 @@ import { NgFor, NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { Route, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-recetas',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './recetas.component.html',
   styleUrl: './recetas.component.css'
 })
@@ -16,10 +18,23 @@ export class RecetasComponent {
   recetas: string[]=['Pure de papa','Espaguetti a la carbonara','Licuado de mango'];
   audio = new Audio('assets/poyoyo.mp3');
   id=0;
-  botonid(){
-    this.id = this.id+1;
-  }
+
+  precios: { [key: string]: number } = {
+    'Pure de papa': 60,
+    'Espaguetti a la carbonara': 180,
+    'Licuado de mango': 30
+  };
+  cantidades: { [key: string]: number } = {
+    'Pure de papa': 0,
+    'Espaguetti a la carbonara': 0,
+    'Licuado de mango': 0
+  };
+  total: number = 0;
+
+  inputData: string = '';
+
   constructor(private router: Router){}
+
 
   inicio(){
     this.router.navigate(['/inicio']);
@@ -37,21 +52,10 @@ export class RecetasComponent {
     this.audio.play();
   }
 
-  redirect(id: any): void {
-    
-    switch (id) {
-      case '1':
-        this.router.navigate(['/recetas/pure-de-papas']);
-        break;
-      case '2':
-        this.router.navigate(['/recetas/espaguetti-a-la-carbonara']);
-        break;  
-      case '3':
-        this.router.navigate(['/recetas/licuado-de-mango']);
-        break;
-      default:
-        break;
-    }
+  calcularTotalGeneral() {
+    this.total = this.recetas.reduce((acc, receta) => {
+      return acc + (this.cantidades[receta] * this.precios[receta]);
+    }, 0);
   }
 
 }
